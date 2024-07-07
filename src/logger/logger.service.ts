b1344a4ac82e12b269693dde1cfd793fb12c2cf2
Abdulloh <abduvohidov.abdullokh@gmail.com>
2024-07-07 14:55:28 +0500

    eslint & prettier had connected and used into project. nodemon too.

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,27 +1,27 @@
-import { Logger } from "tslog";
-import { ILogger } from "./logger.interface";
-import { injectable } from "inversify";
-import "reflect-metadata"
+import { Logger } from 'tslog';
+import { ILogger } from './logger.interface';
+import { injectable } from 'inversify';
+import 'reflect-metadata';
 
 @injectable()
 export class LoggerService implements ILogger {
-  private logger: Logger<ILogger>;
+	private logger: Logger<ILogger>;
 
-  constructor() {
-    this.logger = new Logger({
-      name: "MyLogger",
-      minLevel: 1,
-      prettyLogTemplate: "{{logLevelName}} ",
-    });
-  }
+	constructor() {
+		this.logger = new Logger({
+			name: 'MyLogger',
+			minLevel: 1,
+			prettyLogTemplate: '{{logLevelName}} ',
+		});
+	}
 
-  log(...args: unknown[]) {
-    this.logger.info(...args);
-  }
-  error(...args: unknown[]) {
-    this.logger.error(...args);
-  }
-  warn(...args: unknown[]) {
-    this.logger.warn(...args);
-  }
+	log(...args: unknown[]): void {
+		this.logger.info(...args);
+	}
+	error(...args: unknown[]): void {
+		this.logger.error(...args);
+	}
+	warn(...args: unknown[]): void {
+		this.logger.warn(...args);
+	}
 }
